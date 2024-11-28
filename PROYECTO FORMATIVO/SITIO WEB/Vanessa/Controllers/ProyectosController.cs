d9bb731bae51cf74377d5be20d93b4fb698bb3c3
AliMedina18 <alicia_medina@soy.sena.edu.co>
2024-11-27 22:57:18 -0500

    Create

---
+++ PROYECTO FORMATIVO/SITIO WEB/Vanessa/Controllers/ProyectosController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Threading.Tasks;
+using Vanessa.Data;
+using Vanessa.Models;
+
+namespace MiAppPdfCrud.Controllers
+{
+    public class ProyectosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly string _uploadPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+
+        public ProyectosController(ApplicationDbContext context)
+        {
+            _context = context;
+            if (!Directory.Exists(_uploadPath))
+            {
+                Directory.CreateDirectory(_uploadPath);
+            }
+        }
+
+        // GET: Proyectos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proyectos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,EquiposInvestigacion,FechaInicio")] Proyecto proyecto, IFormFile pdfFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (pdfFile != null && pdfFile.Length > 0)
+                {
+                    var fileName = Path.GetFileName(pdfFile.FileName);
+                    var filePath = Path.Combine(_uploadPath, fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await pdfFile.CopyToAsync(stream);
+                    }
+                    proyecto.DocumentoProyecto = fileName;
+                }
+
+                _context.Add(proyecto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyectos/Index
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Proyectos.ToListAsync());
+        }
+
+        // GET: Proyectos/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyectos/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: Proyectos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,EquiposInvestigacion,FechaInicio")] Proyecto proyecto, IFormFile pdfFile)
+        {
+            if (id != proyecto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (pdfFile != null && pdfFile.Length > 0)
+                    {
+                        var fileName = Path.GetFileName(pdfFile.FileName);
+                        var filePath = Path.Combine(_uploadPath, fileName);
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await pdfFile.CopyToAsync(stream);
+                        }
+                        proyecto.DocumentoProyecto = fileName;
+                    }
+
+                    _context.Update(proyecto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Proyectos.Any(e => e.Id == proyecto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyectos/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto == null)
+            {
+                return NotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: Proyectos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto != null)
+            {
+                var filePath = Path.Combine(_uploadPath, proyecto.DocumentoProyecto);
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+
+                _context.Proyectos.Remove(proyecto);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Proyectos/Download/5
+        public async Task<IActionResult> Download(int id)
+        {
+            var proyecto = await _context.Proyectos.FindAsync(id);
+            if (proyecto == null || string.IsNullOrEmpty(proyecto.DocumentoProyecto))
+            {
+                return NotFound();
+            }
+
+            var filePath = Path.Combine(_uploadPath, proyecto.DocumentoProyecto);
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            return File(fileBytes, "application/pdf", proyecto.DocumentoProyecto);
+        }
+    }
+}
