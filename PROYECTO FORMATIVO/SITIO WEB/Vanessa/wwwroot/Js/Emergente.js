40fb3d5f1c88b65f9d8b54adc7c15ea01af4793d
AliMedina18 <alicia_medina@soy.sena.edu.co>
2024-11-27 23:26:08 -0500

    Delete

--- PROYECTO FORMATIVO/SITIO WEB/Vanessa/wwwroot/Js/Emergente.js
+++
@@ -1,9 +0,0 @@
- // Mostrar la ventana emergente al cargar la página
- window.onload = function() {
-    document.getElementById('popup').style.display = 'flex';
-};
-
-// Función para cerrar la ventana emergente
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
